023487174ea071daf3092f8fc0098181f7e24b42
carlogino <carloginocatapang@gmail.com>
2020-06-21 16:15:58 +0800

    Added ngrx router store via ng add

--- frontend/projects/personal/src/app/app.module.ts
+++ frontend/projects/personal/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { environment } from '../environments/environment';
       metaReducers,
     }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
