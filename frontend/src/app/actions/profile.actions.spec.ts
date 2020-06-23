8ea732d27a704b89b9d98456478dfd7840a9badb
carlogino <carloginocatapang@gmail.com>
2020-06-23 23:46:50 +0800

    Updated compilation error

--- frontend/src/app/actions/profile.actions.spec.ts
+++ frontend/src/app/actions/profile.actions.spec.ts
@@ -2,6 +2,8 @@ import * as fromProfile from './profile.actions';
 
 describe('loadProfiles', () => {
   it('should return an action', () => {
-    expect(fromProfile.loadProfiles().type).toBe('[Profile] Load Profiles');
+    expect(fromProfile.updateProfileName({ name: '' }).type).toBe(
+      fromProfile.ProfileAction.UpdateProfileName,
+    );
   });
 });
