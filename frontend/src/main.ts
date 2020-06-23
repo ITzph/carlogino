8cddf16720fc1d841d0f3818561f029b43713463
carlogino <carloginocatapang@gmail.com>
2020-06-23 19:34:14 +0800

    Added HMR

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,11 +4,31 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { hmrModule } from '@angularclass/hmr';
+
 if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-});
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  // @ts-ignore
+  if (module.hot) {
+    bootstrap()
+      .then((ngModuleRef) => {
+        // @ts-ignore
+        return hmrModule(ngModuleRef, module);
+      })
+      .catch((err) => console.log(err));
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  document.addEventListener('DOMContentLoaded', () => {
+    bootstrap()
+    .catch(err => console.error(err));
+  });
+}
+
