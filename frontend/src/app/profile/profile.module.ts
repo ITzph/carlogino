4fe62e1e5dcad569ef9b6bffc2faa10f1856a32d
carlogino <carloginocatapang@gmail.com>
2020-06-23 21:19:57 +0800

    Integrate NgRx and component

--- frontend/src/app/profile/profile.module.ts
+++ frontend/src/app/profile/profile.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile.component';
+import { Routes, RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import * as fromProfile from '../reducers/profile.reducer';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent,
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [ProfileComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    StoreModule.forFeature(fromProfile.profileFeatureKey, fromProfile.reducer),
+  ],
 })
 export class ProfileModule {}
