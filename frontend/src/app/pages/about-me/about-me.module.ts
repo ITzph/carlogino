e8554cd07696d04e2f0865d3aa2f0cd56656da2c
Israel <israelalisoso1@gmail.com>
2020-07-10 16:31:42 +0800

    'themesComponent'

--- frontend/src/app/pages/about-me/about-me.module.ts
+++ frontend/src/app/pages/about-me/about-me.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AboutMeRoutingModule } from './about-me-routing.module';
 import { AboutMeComponent } from './about-me.component';
+import { ThemesComponent } from './themes/themes.component';
 
 @NgModule({
-  declarations: [AboutMeComponent],
+  declarations: [AboutMeComponent, ThemesComponent],
   imports: [CommonModule, AboutMeRoutingModule],
 })
 export class AboutMeModule {}
