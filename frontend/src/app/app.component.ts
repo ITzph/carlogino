9ef1d9d115654a1c75d3a9cea1babdf473afd4c3
rangerisrael <38226544+rangerisrael@users.noreply.github.com>
2020-07-11 15:20:17 +0800

    Feature/about me (#15)
    * 'ChangeTitle'
    
    * 'ChangingDbFile'
    
    * 'Modified-model'
    
    * 'themesComponent'
    
    * ProfileandTestimonial'
    
    * 'assets-images-file'
    
    * 'AddingDataOn-themesComponent'
    
    * 'ProfileTemplate'
    
    * 'ProfileServices'
    
    * 'TestimonialService'
    
    * 'Debug'
    
    * 'ChangingData'
    
    * 'AngularMaterial'
    
    * 'ChangingData'
    
    * 'Changing-profile-property-into-image'
    
    * 'Removing-margin-into-about-component-css'
    
    * 'Importing-variable'
    
    * 'Replicating-html-element-into-css-class-selector
    
    * 'debugging-state'

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'frontend';
+  title = 'Website 2020';
 
   public ngOnInit(): void {}
 }
---
+++ frontend/src/app/models/profile-model.ts
@@ -0,0 +1,8 @@
+export class Profiles {
+  name: string;
+  image: String;
+  address: string;
+  currentCompany: string;
+  currentRole: String;
+  testimonial: String;
+}
